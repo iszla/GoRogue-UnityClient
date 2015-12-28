9f9b2199fa9b79ea52bc1914416d847c8bd9fe43
Karl Hesselgren <karl.hesselgren@freespee.com>
2015-12-28 20:34:38 +0100

    Moved .scene and updated the player and camera movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,19 +3,47 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
+	Vector3 movement;
+	Vector3 cameraMovement;
 
-	Vector3 playerMovement;
-
-	// Use this for initialization
 	void Start() {
-		playerMovement = Vector3.zero;
+		
 	}
-	
-	// Update is called once per frame
-	void Update() {
-		playerMovement = new Vector3( Input.GetAxisRaw( "Horizontal" ), Input.GetAxisRaw( "Vertical" ), 0 );
 
-		this.transform.position += playerMovement;
-		Camera.main.transform.position = new Vector3( this.transform.position.x, this.transform.position.y, -10 );
+	void Update() {
+
+		MovePlayer();
+		UpdateCamera();
+
+	}
+
+	void MovePlayer() {
+		movement = Vector3.zero;
+
+		if ( Input.GetKeyDown( KeyCode.LeftArrow ) || Input.GetKeyDown( KeyCode.A ) )
+			movement.x = -1;
+		else if ( Input.GetKeyDown( KeyCode.RightArrow ) || Input.GetKeyDown( KeyCode.D ) )
+			movement.x = 1;
+		else if ( Input.GetKeyDown( KeyCode.UpArrow ) || Input.GetKeyDown( KeyCode.W ) )
+			movement.y = 1;
+		else if ( Input.GetKeyDown( KeyCode.DownArrow ) || Input.GetKeyDown( KeyCode.S ) )
+			movement.y = -1;
+
+		this.transform.position += movement;
+	}
+
+	void UpdateCamera() {
+		cameraMovement = Camera.main.transform.position;
+
+		if ( this.transform.position.x - Camera.main.transform.position.x < -2 )
+			cameraMovement.x -= 1;
+		else if ( this.transform.position.x - Camera.main.transform.position.x > 2 )
+			cameraMovement.x += 1;
+		else if ( this.transform.position.y - Camera.main.transform.position.y < -2 )
+			cameraMovement.y -= 1;
+		else if ( this.transform.position.y - Camera.main.transform.position.y > 2 )
+			cameraMovement.y += 1;
+
+		Camera.main.transform.position = new Vector3( cameraMovement.x, cameraMovement.y, -10 );
 	}
 }
